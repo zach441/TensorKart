cc48a44e69bc95f1fde9738c6188b0f4c48f67a3
Zach Oakes <zachoakes96@gmail.com>
2018-05-03 23:48:11 -0500

    Fixed train to work with python3 naming conventions

--- train.py
+++ train.py
@@ -6,7 +6,7 @@ from keras.layers import Dense, Dropout, Flatten
 from keras.layers import Conv2D
 from keras import optimizers
 from keras import backend as K
-from utils import Sample
+from TensorKart.utils import Sample
 
 # Global variable
 OUT_SHAPE = 5
